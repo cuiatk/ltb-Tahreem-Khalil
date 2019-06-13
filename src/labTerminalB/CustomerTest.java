aa14e7bb2a5f3252dd695fca419c23ab276d740b
Tahreem-Khalil <tahreem.khalil173@gmail.com>
2019-06-13 10:29:33 +0500

    wrote two testcases for childern class

--- src/labTerminalB/CustomerTest.java
+++ src/labTerminalB/CustomerTest.java
@@ -1,10 +1,13 @@
 package labTerminalB;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class CustomerTest {
 	Movie m1 = new Movie("Spancer", 0);
 	Movie m2 = new Movie("Speed", 1);
+	Movie m3 = new Movie("the shape of water", 2);
 	Customer me = new Customer("Nadir");
 	Customer me2 = new Customer("Allah Ditta");
 	Rental r1 = new Rental(m1, 23);
@@ -32,5 +35,26 @@ public class CustomerTest {
 	 * strategy basics i.e. input partition etc
 	 */
 	
+	/**
+	 *  test 2 customer rents one  new release movie 
+	 */
+	  @Test
+	    public void basicNewReleaseRental() {
+	        me2.addRental(new Rental(m2, 1));
+	        assertEquals(me2.statement(), me2.statement());
+	    }
+	  
+	  /**
+	   * test 3 customer rents all three rental movies regular, new release, and children
+	   */
+	   @Test
+	    public void shouldSumVariousRentals() {
+	        me2.addRental(new Rental(m2, 2));
+	        me2.addRental(new Rental(m1, 1));
+	        me2.addRental(new Rental(m3, 3));
+	        assertEquals(me2.statement(), me2.statement());
+	    }
 
 }
+
+
